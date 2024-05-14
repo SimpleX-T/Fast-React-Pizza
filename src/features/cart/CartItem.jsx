b6cfaf0cc49ec5fd236d61959468f7d1b27899a6
Xcodes <ntmark2004@gmail.com>
2024-05-14 09:20:43 +0100

    commit message

--- src/features/cart/CartItem.jsx
+++ src/features/cart/CartItem.jsx
@@ -1,9 +1,11 @@
+import { useDispatch } from "react-redux";
 import { formatCurrency } from "../../utils/helpers";
 import DeleteItemButton from "./DeleteItemButton";
 import UpdateItemQuantity from "./UpdateItemQuantity";
 
 function CartItem({ item }) {
 	const { pizzaId, name, quantity, totalPrice } = item;
+	const dispatch = useDispatch();
 
 	return (
 		<li className='py-3 sm:flex sm:items-center sm:justify-between'>
@@ -14,7 +16,7 @@ function CartItem({ item }) {
 				<p className='text-sm font-bold'>
 					{formatCurrency(totalPrice)}
 				</p>
-				<UpdateItemQuantity action='+' />
+				<UpdateItemQuantity action='+' onClick={} />
 				<UpdateItemQuantity action='-' />
 				<DeleteItemButton pizzaId={pizzaId} />
 			</div>
