23419af31f483e9be7aee334ed0f4d364fa817f6
Xcodes <ntmark2004@gmail.com>
2024-05-14 09:37:28 +0100

    commit message

--- src/features/cart/UpdateItemQuantity.jsx
+++ src/features/cart/UpdateItemQuantity.jsx
@@ -2,7 +2,7 @@ function UpdateItemQuantity({ action, onClick }) {
 	return (
 		<button
 			className='w-5 h-5 flex justify-center items-center bg-slate-800 rounded-full hover:bg-slate-600 transition-colors duration-300'
-			onClick={onClick()}>
+			onClick={onClick}>
 			<span className='text-white cursor-pointer'>{action}</span>
 		</button>
 	);
