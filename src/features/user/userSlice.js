d65a3154ecb3eb1c9e04a86d19ca983366dbfe69
Xcodes <ntmark2004@gmail.com>
2024-05-16 21:23:45 +0100

    fetching customers address using reverse-geocoding

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -1,6 +1,36 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { getAddress } from "../../services/apiGeocoding";
 
-const initialState = { userName: "" };
+const initialState = {
+	userName: "",
+	status: "idle",
+	position: {},
+	address: "",
+	error: "",
+};
+
+function getPosition() {
+	return new Promise(function (resolve, reject) {
+		navigator.geolocation.getCurrentPosition(resolve, reject);
+	});
+}
+
+const fetchAddress = createAsyncThunk("user/fetchAddress", async function () {
+	// 1) We get the user's geolocation position
+	const positionObj = await getPosition();
+	const position = {
+		latitude: positionObj.coords.latitude,
+		longitude: positionObj.coords.longitude,
+	};
+
+	// 2) Then we use a reverse geocoding API to get a description of the user's address, so we can display it the order form, so that the user can correct it if wrong
+	const addressObj = await getAddress(position);
+	const address = `${addressObj?.locality}, ${addressObj?.city} ${addressObj?.postcode}, ${addressObj?.countryName}`;
+
+	// 3) Then we return an object with the data that we are interested in
+	// the returned result will be the 'action.payload'
+	return { position, address };
+});
 
 const userSlice = createSlice({
 	name: "user",
@@ -10,6 +40,21 @@ const userSlice = createSlice({
 			state.userName = action.payload;
 		},
 	},
+	extraReducers: (builder) =>
+		builder
+			.addCase(
+				fetchAddress.pending,
+				(state, action) => (state.status = "loading")
+			)
+			.addCase(fetchAddress.fulfilled, (state, action) => {
+				state.status = "idle";
+				state.position = action.payload.position;
+				state.address = action.payload.address;
+			})
+			.addCase(fetchAddress.rejected, (state, action) => {
+				state.status = "error";
+				state.error = action.error.message;
+			}),
 });
 
 export const { updateName } = userSlice.actions;
@@ -18,27 +63,3 @@ export default userSlice.reducer;
 export const getUsername = (state) => state.user.userName;
 
 export const getUserFirstname = (state) => state.user.userName.split(" ")[0];
-
-/*
-function getPosition() {
-  return new Promise(function (resolve, reject) {
-    navigator.geolocation.getCurrentPosition(resolve, reject);
-  });
-}
-
-async function fetchAddress() {
-  // 1) We get the user's geolocation position
-  const positionObj = await getPosition();
-  const position = {
-    latitude: positionObj.coords.latitude,
-    longitude: positionObj.coords.longitude,
-  };
-
-  // 2) Then we use a reverse geocoding API to get a description of the user's address, so we can display it the order form, so that the user can correct it if wrong
-  const addressObj = await getAddress(position);
-  const address = `${addressObj?.locality}, ${addressObj?.city} ${addressObj?.postcode}, ${addressObj?.countryName}`;
-
-  // 3) Then we return an object with the data that we are interested in
-  return { position, address };
-}
-*/
